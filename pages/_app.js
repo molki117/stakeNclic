88d8dd8c1f23b57faea0851887d5c58afb5328d0
molki117 <146346212+molki117@users.noreply.github.com>
2025-01-31 11:47:11 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'; // Import du fichier global de styles
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
