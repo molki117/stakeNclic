5cc8f9ebaca66e93d8f8c707d9a5dfb4225c4e3e
molki117 <146346212+molki117@users.noreply.github.com>
2025-01-31 12:08:56 +0100

    Update Bouttons.js

--- components/Bouttons.js
+++ components/Bouttons.js
@@ -1,16 +1,16 @@
 // components/Buttons.js
-import React from 'react';
+import React, { useState } from 'react';
 
-const Buttons = () => {
+const Buttons = ({ isWalletConnected }) => {
   return (
     <div className="buttons-container">
-      <button className="button stake-button" disabled>
+      <button className="button stake-button" disabled={!isWalletConnected}>
         Stake
       </button>
-      <button className="button claim-button" disabled>
+      <button className="button claim-button" disabled={!isWalletConnected}>
         Claim
       </button>
-      <button className="button clic-button" disabled>
+      <button className="button clic-button" disabled={!isWalletConnected}>
         Clic
       </button>
     </div>
