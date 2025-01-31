c1c56f5bb0e25832956d95973abf80ef96ad96d9
molki117 <146346212+molki117@users.noreply.github.com>
2025-01-31 10:45:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>StakeNclic</h1>
+      <ConnectButton /> {/* Bouton de connexion */}
+    </div>
+  );
+}
